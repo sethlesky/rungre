616ddc6d30c5bcd877416b71e2ff637528d32407
Seth Lesky <sethlesky@gmail.com>
2013-01-20 16:17:27 -0500

    Text cycle working without jumps, inline-block fix

--- src/documents/scripts/txtcycle.js
+++ src/documents/scripts/txtcycle.js
@@ -7,7 +7,7 @@
     function showNextQuote() {
         ++quoteIndex;
         quotes.eq(quoteIndex % quotes.length)
-            .fadeIn(2000)
+            .fadeIn(2000).css("display","inline-block")
             .delay(2000)
             .fadeOut(2000, showNextQuote);
     }
